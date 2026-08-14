718a6936f518b357750b65ff9f99d77ddf6c4833
SirSpin <pmartinsjr87@gmail.com>
2026-08-14 16:50:13 -0300

    R7.5.3C: make spincore_nn package imports lazy for torch-free codecs

--- python/spincore_nn/__init__.py
+++ python/spincore_nn/__init__.py
@@ -1,4 +1,37 @@
-from .models import NetworkConfig,AdvantageNet,AveragePolicyNet
-from .reservoir import UniformReservoir,AdvantageSample,StrategySample
-from .bundle import DomainBundle
-__all__=['NetworkConfig','AdvantageNet','AveragePolicyNet','UniformReservoir','AdvantageSample','StrategySample','DomainBundle']
+from __future__ import annotations
+
+from importlib import import_module
+
+__all__ = [
+    "NetworkConfig",
+    "AdvantageNet",
+    "AveragePolicyNet",
+    "UniformReservoir",
+    "AdvantageSample",
+    "StrategySample",
+    "DomainBundle",
+]
+
+_EXPORTS = {
+    "NetworkConfig": (".models", "NetworkConfig"),
+    "AdvantageNet": (".models", "AdvantageNet"),
+    "AveragePolicyNet": (".models", "AveragePolicyNet"),
+    "UniformReservoir": (".reservoir", "UniformReservoir"),
+    "AdvantageSample": (".reservoir", "AdvantageSample"),
+    "StrategySample": (".reservoir", "StrategySample"),
+    "DomainBundle": (".bundle", "DomainBundle"),
+}
+
+
+def __getattr__(name: str):
+    try:
+        module_name, attribute = _EXPORTS[name]
+    except KeyError as exc:
+        raise AttributeError(name) from exc
+    value = getattr(import_module(module_name, __name__), attribute)
+    globals()[name] = value
+    return value
+
+
+def __dir__():
+    return sorted(set(globals()) | set(__all__))
