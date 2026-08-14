de333c4244187fa2b56eba14fb3f0248991c8549
SirSpin <pmartinsjr87@gmail.com>
2026-08-13 23:34:23 -0300

    test: cover explicit forced posts and pot-before history

--- tests/test_betting_engine.cpp
+++ tests/test_betting_engine.cpp
@@ -3,6 +3,7 @@
 #include "spincore/betting_engine.hpp"
 using namespace spincore;
 SPIN_TEST(betting_posts_3h_blinds){auto s=sc3(0);BettingEngine b(s,make_game_topology(s));REQUIRE(b.pot()==30);REQUIRE(b.actor()==0);REQUIRE(b.players()[1].street_commitment==10);REQUIRE(b.players()[2].street_commitment==20);}
+SPIN_TEST(betting_history_marks_blinds_forced){auto s=sc3(0);BettingEngine b(s,make_game_topology(s));REQUIRE(b.history().size()==2);REQUIRE(b.history()[0].forced);REQUIRE(b.history()[1].forced);REQUIRE(b.history()[0].pot_before==0);REQUIRE(b.history()[0].pot_after==10);REQUIRE(b.history()[1].pot_before==10);REQUIRE(b.history()[1].pot_after==30);auto l=b.legal_actions(0);auto ev=b.apply(0,{ExactActionType::RaiseTo,l.min_raise_to});REQUIRE(!ev.forced);REQUIRE(ev.pot_before==30);REQUIRE(ev.pot_after==b.pot());}
 SPIN_TEST(betting_hu_button_acts_first){auto s=schu(1);BettingEngine b(s,make_game_topology(s));REQUIRE(b.actor()==1);REQUIRE(b.legal_actions(1).to_call==10);}
 SPIN_TEST(betting_fold_ends_hu){auto s=schu(1);BettingEngine b(s,make_game_topology(s));b.apply(1,{ExactActionType::Fold,0});REQUIRE(b.hand_over_by_fold());REQUIRE(b.nonfolded_count()==1);}
 SPIN_TEST(betting_call_check_closes_preflop){auto s=schu(1);BettingEngine b(s,make_game_topology(s));b.apply(1,{ExactActionType::Call,0});REQUIRE(b.actor()==2);b.apply(2,{ExactActionType::Check,0});REQUIRE(b.street_complete());}
