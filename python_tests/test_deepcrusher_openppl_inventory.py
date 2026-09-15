ea097e59eb49b7a2f6ffe22b0b28450a8e79f99e
SirSpin <pmartinsjr87@gmail.com>
2026-09-15 17:40:29 -0300

    Fix DeepCrusher OpenPPL inventory test syntax

--- python_tests/test_deepcrusher_openppl_inventory.py
+++ python_tests/test_deepcrusher_openppl_inventory.py
@@ -6,14 +6,13 @@ from spincore.deepcrusher_openppl import parse_openppl_source
 def test_openppl_dependency_inventory(tmp_path: Path):
     source_path = tmp_path / "mini.txt"
     source_path.write_text(
-        """##notes##\nmini\n"
+        "##notes##\nmini\n"
         "##f$preflop##\nWhen f$helper && hand$A Return BetMax Force\n"
         "##f$flop##\nWhen user_plan && f$helper Return Call Force\n"
         "##f$turn##\nf$helper\n"
         "##f$river##\nWhen Others Fold Force\n"
         "##f$helper##\nf$native_external && user_plan\n"
-        "##list_A##\nAA KK AKs\n"
-        ,
+        "##list_A##\nAA KK AKs\n",
         encoding="utf-8",
     )
     source = parse_openppl_source(source_path)
